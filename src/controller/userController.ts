3c97cccc19355c70ee02424a292cf7bca38a2399
gsBullet <glmsorowar@gmail.com>
2025-03-03 11:58:15 +0600

    store profile data

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -4,6 +4,7 @@ import DataModel from "../model/userModel";
 import moment from "moment/moment";
 import fs from "fs";
 import path from "path";
+import bcrypt from "bcrypt";
 
 export default function (fastify: FastifyInstance) {
   return {
@@ -13,27 +14,57 @@ export default function (fastify: FastifyInstance) {
     },
 
     store: async function (req: FastifyRequest, res: FastifyReply) {
-      const file = await req.file();
       const { user_id, name, email, password, image } = req.body as any;
-      // console.log(image);
+
+      // **Hash the password using bcrypt**
+      const saltRounds = 10;
+      const hashedPassword = await bcrypt.hash(password.value, saltRounds);
+
+      let data = {
+        user_id: Number(user_id.value),
+        name: String(name.value),
+        email: String(email.value),
+        password: String(hashedPassword),
+      };
+      console.log(data);
+      // return res
+      //   .status(200)
+      //   .send({ data, message: "user created successfully" });
+
       if (!image) {
         return res.status(400).send({ error: "No file uploaded" });
       }
 
-      const uploadDir = path.join(__dirname, "../uploads"); // Set upload directory
+      // Create uploads directory if it doesn't exist
+      const uploadDir = path.join(__dirname, "../uploads");
       if (!fs.existsSync(uploadDir)) {
         fs.mkdirSync(uploadDir, { recursive: true });
       }
 
-      const filePath = path.join(
-        uploadDir,
-        moment().format("YYYYMMDDHHmmss") + image.value.ext
-      );
-      const writeStream = fs.createWriteStream(filePath);
-      await image.file.pipe(writeStream); // Save file
+      // Handle file upload
+      let imagePath = "";
+      if (image?.file) {
+        const filename = `${Date.now()}-${image.filename}`;
+        imagePath = path.join(uploadDir, filename);
+        await fs.promises.writeFile(imagePath, await image.toBuffer());
+      }
+      const relativePath = imagePath.split("/fastify/")[1];
+
       try {
-        // const user = await DataModel.create({ user_id, name, email, password });
-        return res.status(200).send({filePath,writeStream, message: "user created successfully" });
+        const user = await DataModel.create(data);
+        user.image = relativePath;
+        await user.save();
+        // image: relativePath,
+        if (user) {
+          return res.status(200).send({
+            user,
+            message: "user created successfully",
+          });
+        } else {
+          return res.status(400).send({
+            message: "user not created successfully",
+          });
+        }
       } catch (error) {
         return res.status(500).send({ error });
       }
